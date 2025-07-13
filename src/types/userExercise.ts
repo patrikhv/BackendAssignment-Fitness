215b9514014884d119f13efdae0341023877eca1
Patrik Hvozdovič <hvozdovic.patrik@gmail.com>
2025-07-13 20:12:58 +0200

    fix: userExercise completedAt zod type fix

--- src/types/userExercise.ts
+++ src/types/userExercise.ts
@@ -1,7 +1,9 @@
 import z from "zod";
 
 export const UserExerciseRequestSchema = z.object({
-    completedAt: z.date(),
+    completedAt: z.iso
+        .datetime({message: "Completed at must be a valid ISO date"})
+        .transform((val) => new Date(val)),
     duration: z.number().int().positive("Duration must be a positive integer")
 });
 
